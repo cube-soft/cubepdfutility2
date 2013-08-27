b2325c97cceda778e5a508b94edcfe22781e51ab
tamada <s.sub.sol@aol.com>
2013-08-27 16:39:57 +0900

    add files

---
+++ CubePdfUtility2/StartMessage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Windows.Controls.Ribbon;
+
+namespace CubePdfUtility
+{
+    /// <summary>
+    /// Interaction logic for StartMessage.xaml
+    /// </summary>
+    public partial class StartMessage : Canvas
+    {
+        public StartMessage()
+        {
+            InitializeComponent();
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// AddRecentFiles
+        ///
+        /// <summary>
+        /// StartMessageに最近開いたファイルを追加します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void AddRecentFiles(IList<string> recents)
+        {
+            for (int i = 0; i < recents.Count; ++i)
+            {
+                var gallery = new RibbonGalleryItem();
+                gallery.Content = String.Format("{0} {1}", i + 1, System.IO.Path.GetFileName(recents[i]));
+                gallery.Tag = recents[i];
+                RecentFilesGallery.Items.Add(gallery);
+            }
+        }
+    }
+}
