81345403d0dbe8f2950941f49568ae60ed2868bd
ichikawa <i.shu0408@gmail.com>
2013-12-17 20:02:00 +0900

    BorderThicknessConverterの実装＋Margin等を考慮したサムネイルｍのWidthのConverterの実装途中

---
+++ CubePdfUtility2/BorderThicknessConverter.cs
@@ -0,0 +1,84 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// ItemHeightConverter.cs
+///
+/// Copyright (c) 2013 CubeSoft, Inc. All rights reserved.
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Affero General Public License as published
+/// by the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Affero General Public License for more details.
+///
+/// You should have received a copy of the GNU Affero General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+using System.Windows.Data;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace CubePdfUtility
+{
+    class BorderThicknessConverter : IMultiValueConverter
+    {
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Convert
+        /// 
+        /// <summary>
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                _backs= new object[values.Length];
+                for (int i = 0; i < values.Length; i++)
+                {
+                    _backs[i] = values[i];
+                }
+                var itemWidth = (double)values[0];
+                var viewItemWidth = (double)values[1];
+                var itemHeight = (double)values[2];
+                var viewItemHeight = (double)values[3];
+                double ratioW = itemWidth / viewItemWidth;
+                double ratioH = itemHeight / viewItemHeight;
+                return new System.Windows.Thickness(ratioW > ratioH ? ratioW : ratioH);
+            }
+            catch (Exception /* err */) { return new System.Windows.Thickness(1.0); }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ConvertBack
+        /// 
+        /// <summary>
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                object[] vals = new object[_backs.Length];
+                for (int i = 0; i < _backs.Length; i++)
+                {
+                    vals[i] = _backs[i];
+                }
+                return vals;
+            }
+            catch (Exception /* err */) { return new object[]{}; }
+        }
+
+        #region Static variables
+        private static object[] _backs;
+        #endregion
+    }
+}
